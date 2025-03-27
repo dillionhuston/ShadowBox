3ddcd11854d6e25e426f5e981fb0d9ffc94af5ac
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-03-27 16:14:07 +0000

    website update

--- backend/auth.py
+++ backend/auth.py
@@ -4,11 +4,9 @@ import bcrypt
 
 class auth():
      def hash_password(self, password):
-        # It's a good idea to use bcrypt rather than sha256 for hashing passwords
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(password.encode(), salt)
         return hashed.decode('utf-8')
 
      def verify_password(self, stored_hash, entered_password):
-        # Verify password using bcrypt
         return bcrypt.checkpw(entered_password.encode(), stored_hash.encode())
