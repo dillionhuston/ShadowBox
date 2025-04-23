095abcb5de3e8ef17a98e20f0e58cfb8da2f4cfe
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-04-23 21:52:23 +0100

    Update file.py

--- Shadow. V2/app/models/file.py
+++ Shadow. V2/app/models/file.py
@@ -1,23 +1,32 @@
 from . import db
-from flask_sqlalchemy import SQLAlchemy
+import uuid
+from flask_login import current_user
 
 class File(db.Model):
     __tablename__ = "files"
     file_id = db.Column(db.String(36), primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner_id = db.Column(db.String(64), db.ForeignKey('user.id'), nullable=False)
     file_path = db.Column(db.String(500), nullable=False)
     file_name = db.Column(db.String(300), nullable=False)
 
-    def add_file(filename: str, filepath: str, file_id: str):
-        newfile = File(file_path=filepath, file_name=filename, file_id=file_id)
+    @staticmethod
+    def add_file(filepath:str, filename:str):
+        newfile = File(
+            file_id=str(uuid.uuid4()),
+            owner_id=current_user.id,
+            file_path=filepath, 
+            file_name=filename
+        )
+        print(newfile.file_id)
         db.session.add(newfile)
         db.session.commit()
         return newfile
     
+    @staticmethod
     def remove_file(file_id: str) -> bool:
         file = File.query.filter_by(file_id=file_id).first()
         if file is None:
             return False
-        db.session.delete(file)  # Fixed typo from 'delele'
+        db.session.delete(file)
         db.session.commit()
         return True
