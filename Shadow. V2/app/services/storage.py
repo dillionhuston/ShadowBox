7b0ed4243f12227acd7e3bf42ba8bcef92b536f2
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-04-23 19:08:34 +0100

    File storage Class

--- Shadow. V2/app/services/storage.py
+++ Shadow. V2/app/services/storage.py
@@ -1,11 +1,24 @@
 #file sotrage and retrival logic
+import logging
+import os 
+from config import Config
+
+encrypted_file_path = Config.ENCRYPTED_FILE_PATH  
 
 
 class FileStorageService:
-    def save_file(self, file_data, file_name):
-        # Logic to store encrypted files
-        pass
 
+    @staticmethod
+    def save_file(self, file_data):
+        if file is None:
+            logging.error("no file")
+            return
+        else:
+            path = os.path.join(filename)
+            with open(encrypted_file_path, 'wb') as file:
+                file.write(file_data)
+        return
+      
     def retrieve_file(self, file_id):
         # Logic to retrieve files
         pass
